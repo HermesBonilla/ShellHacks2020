6fd9cc23bbc8996a27c13edfe0a49b725d24a37b
jquitgra <45401768+jquitgra@users.noreply.github.com>
2020-09-12 20:22:07 -0400

    outline to setup up our data

---
+++ imageDeres.py
@@ -0,0 +1,16 @@
+import PIL
+import os
+
+def main():
+    out_dir = ""
+    images_dir = ""
+    for image in os.listdir(images_dir):
+        if(image.endswith(".jpg")):
+            im = PIL.Image.open(os.path.join(images_dir, image))
+            dims = im.size
+            hgt = dims[0]*0.5
+            wit = dims[1]*0.5
+            im = im.resize((hgt,wit),PIL.Image.ANTIALIAS)
+            im.save(out_dir, quality=95)
+if __name__ == '__main__':
+    main()
