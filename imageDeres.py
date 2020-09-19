6997325601acb692d292f26caa06d416c184c9c7
Hermes Bonilla <hboni008@fiu.edu>
2020-09-19 10:25:54 -0400

    Modified variable names for clarity

--- imageDeres.py
+++ imageDeres.py
@@ -1,19 +1,22 @@
-import PIL
+from PIL import Image
 import os
 
 def main():
-"""
-Creates noise in our dataset to train the denoising model
-"""
+    """
+    Creates noise in our dataset to train the de-noising model
+    """
     out_dir = ""
     images_dir = ""
     for image in os.listdir(images_dir):
-        if(image.endswith(".jpg")):
-            im = PIL.Image.open(os.path.join(images_dir, image))
-            dims = im.size
+        if(image.endswith(".jpg") or image.endswith(".png") or image.endswith(".jpeg")):
+            img = Image.open(os.path.join(images_dir, image))
+            dims = img.size
             hgt = dims[0]*0.5
             wit = dims[1]*0.5
-            im = im.resize((hgt,wit),PIL.Image.ANTIALIAS)
-            im.save(out_dir, quality=95)
+            CompressResult = img.resize((hgt,wit),Image.ANTIALIAS)
+            DecompressResult = "REPLACE - DO NOT USE LIKE THIS"
+            DecompressResult.save(out_dir, quality=95)
+
+
 if __name__ == '__main__':
     main()
