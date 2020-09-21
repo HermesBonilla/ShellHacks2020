d96c8162e9c7636295e9879daf6cb8672952ba6c
jquitgra <45401768+jquitgra@users.noreply.github.com>
2020-09-21 00:14:01 -0400

    deres to create data done w arg parse

--- imageDeres.py
+++ imageDeres.py
@@ -1,22 +1,27 @@
 from PIL import Image
+import argparse
+import random
 import os
 
-def main():
+def create(src, dst):
     """
     Creates noise in our dataset to train the de-noising model
     """
-    out_dir = ""
-    images_dir = ""
-    for image in os.listdir(images_dir):
+    for image in os.listdir(src):
         if(image.endswith(".jpg") or image.endswith(".png") or image.endswith(".jpeg")):
-            img = Image.open(os.path.join(images_dir, image))
+            img = Image.open(os.path.join(src, image))
             dims = img.size
-            hgt = dims[0]*0.5
-            wit = dims[1]*0.5
+            fact = random.uniform(0.2, 0.45) 
+            hgt = round(dims[0]*fact)
+            wit = round(dims[1]*fact)
             CompressResult = img.resize((hgt,wit),Image.ANTIALIAS)
-            DecompressResult = "REPLACE - DO NOT USE LIKE THIS"
-            DecompressResult.save(out_dir, quality=95)
+            DecompressResult = CompressResult.resize(img.size, Image.NEAREST)
+            DecompressResult.save(image[:-4]+"_deres.jpg", quality=95)
 
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(description="utility to create deresoulution data")
+    parser.add_argument('-src', required=True, help='src path')
+    parser.add_argument('-dst', required=True, help='dst path')
+    args = parser.parse_args()
+    create(src=args.src, dst=args.dst)
